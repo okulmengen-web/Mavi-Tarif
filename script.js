0903b79ecd788272351102411482b5a525606b23
okulmengen-web <okulmengen@gmail.com>
2026-06-23 15:46:34 +0300

    gemini apı key yeni

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ var emailjsServiceId = 'service_vhwzf9p';
 var emailjsTemplateId = 'template_oz2e5kk';
 var emailjsPublicKey = 'AjU4d6eqgx7iitRmf';
 
-var geminiApiKey = 'AQ.Ab8RN6ID4oZddYzwnn" + "8OqvAOLVwIS-vxlrW7iXYvoZEUbmcF4Q';
+var geminiApiKey = 'var geminiApiKey = 'AQ.Ab8RN6KaGNu2gDbiqpX0jvLSrr_eq-23dWUvtmkYWnH5zrYsGw';
+';
 
 const MASTER_ADMIN = 'okulmengen@gmail.com';
 const ADMIN_EMAILS = ['spotify201122@gmail.com', 'okulmengen@gmail.com'];
